80070229668f1677cdeda18f53ceafa7ddde1bc3
parkchanjong <7000cj@gmail.com>
2019-04-30 22:29:12 +0900

    Updates

---
+++ precache-manifest.1e711273426ea3926bce309df235c2f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce3f334401b1dd84be74",
+    "url": "/movie_/static/js/runtime~main.e21d3da8.js"
+  },
+  {
+    "revision": "c1b0ff9e15eecd9fa943",
+    "url": "/movie_/static/js/main.71db72bc.chunk.js"
+  },
+  {
+    "revision": "f26ae2ba1199b685be6b",
+    "url": "/movie_/static/js/2.1d254b63.chunk.js"
+  },
+  {
+    "revision": "c1b0ff9e15eecd9fa943",
+    "url": "/movie_/static/css/main.a51cdef5.chunk.css"
+  },
+  {
+    "revision": "c2215a61137e717ba89183de84c04eaf",
+    "url": "/movie_/index.html"
+  }
+];
